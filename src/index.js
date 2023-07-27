500344ed9d3649349e72c309265897319df3828c
nadiannis <annisanadianeyla@gmail.com>
2023-07-28 02:27:15 +0700

    chore: move db.js from config folder to utils folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const connectToDb = require('./config/db');
+const connectToDb = require('./utils/db');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 8080;
